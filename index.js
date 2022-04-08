42123badb47d345f659784194991c07450a25920
Nathan Lunn <nathandouglaslunn@gmail.com>
2022-04-08 21:00:46 +0000

    sum-all-number-in-array-test-js.js passes all tests and eslint

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const sumItems = function(array, initialSum = 0) {
+  return array.reduce((sum, num) => {
+    if (Array.isArray(num)) {
+      sum = sumItems(num, sum);
+    } else {
+      sum = sum + num;
+    }
+    return sum;
+  }, initialSum);
+};
 
 module.exports = sumItems;
+
+
+// console.log(sumItems([1, 1, [1, 1, [ 1, 1], 1], 1, 1]))
+// console.log(sumItems([[[4]], [[2]]]))
+// console.log(sumItems([1, 2, 3, 4]))
